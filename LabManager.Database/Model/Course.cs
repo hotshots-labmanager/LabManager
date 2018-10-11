d80c4b46ac585edd5c93612e564e867d1293235d
Sebcar2 <32294957+Sebcar2@users.noreply.github.com>
2018-10-11 14:47:31 +0200

    EF - Course update

--- LabManager.Database/Model/Course.cs
+++ LabManager.Database/Model/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +9,28 @@ namespace LabManager.Database.Model
 {
     public class Course
     {
+        [Key]
         public String Code { get; set; }
         public String Name { get; set; }
         public double Credits { get; set; }
         public int NumberOfStudents { get; set; }
+
+        public Course(String code, String name, double credits, int numberOfStudents)
+        {
+            this.Code = code;
+            this.Name = name;
+            this.Credits = credits;
+            this.NumberOfStudents = numberOfStudents;
+
+        }
+
+        public Course(){
+            }
+
     }
+
+   
+
 
 
 }
