b3ade313eedc3949a0b7980082923360e855d6bf
Joseph <j.erterius@gmail.com>
2018-10-11 12:23:40 +0200

    public view created with slider menu in place

---
+++ LabManager/View/PublicView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LabManager.View
+{
+    /// <summary>
+    /// Interaction logic for PublicView.xaml
+    /// </summary>
+    public partial class PublicView : Window
+    {
+        public PublicView()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
