4b697f65bca7fcf97ebe2028ec5e1e128353beb4
Sebcar2 <32294957+Sebcar2@users.noreply.github.com>
2018-10-12 14:31:51 +0200

    EF - Added TutoringSession + DAL

---
+++ LabManager.Database/DAL/TutoringSessionDAL.cs
@@ -0,0 +1,33 @@
+﻿using LabManager.Database.Context;
+using LabManager.Database.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabManager.Database.DAL
+{
+    public class TutoringSessionDAL
+    {
+        public void AddTutoringSession(TutoringSession ts)
+        {
+            using (var context = new LabManagerDbContext())
+            {
+                context.TutoringSession.Add(ts);
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteTutoringSession(TutoringSession ts)
+        {
+            using (var context = new LabManagerDbContext())
+            {
+                context.TutoringSession.Attach(ts);
+                context.TutoringSession.Add(ts);
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
