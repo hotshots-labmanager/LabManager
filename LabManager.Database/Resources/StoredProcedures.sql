b953918038f71fb700be59bfdffb125673a489e4
Daniel Nilsson <daniel.nilsson94@outlook.com>
2018-10-12 14:15:31 +0200

    Even more changes to stored procedures and triggers

--- LabManager.Database/Resources/StoredProcedures.sql
+++ LabManager.Database/Resources/StoredProcedures.sql
@@ -1,1 +1,28 @@
-﻿
+﻿-- Stored procedure to move a planned tutor session (PlanToTutor) to completed tutoring (HaveTutored)
+DROP PROCEDURE IF EXISTS PlanToTutor_CompleteTutoringSession
+GO
+
+CREATE PROCEDURE PlanToTutor_CompleteTutoringSession
+	@ssn VARCHAR(20),
+	@code VARCHAR(20),
+	@startTime DATETIME,
+	@endTime DATETIME,
+	@hours DECIMAL(5, 2)
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRANSACTION;
+
+		SET NOCOUNT ON;
+		
+		DELETE FROM PlanToTutor WHERE ssn = @ssn AND code = @code AND startTime = @startTime AND endTime = @endTime;
+
+		INSERT INTO HaveTutored VALUES (@ssn, @code, @startTime, @endTime, @hours);
+
+		COMMIT TRANSACTION;
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRANSACTION;
+		THROW
+	END CATCH
+END;
