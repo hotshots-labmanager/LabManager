3784bf385dbf5e72c49bfb2993b8129bbc6c442c
Daniel Nilsson <daniel.nilsson94@outlook.com>
2018-10-21 21:02:00 +0200

    Fixed merge

--- LabManager/Database/Context/LabManagerDbContext.cs
+++ LabManager/Database/Context/LabManagerDbContext.cs
@@ -5,9 +5,9 @@ namespace LabManager.Database.Context
 {
     public class LabManagerDbContext : DbContext
     {
-        public LabManagerDbContext()// : base("LabManagerDbContext")
+        public LabManagerDbContext()
         {
-            //Configuration.ProxyCreationEnabled = false;
+
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -21,32 +21,21 @@ namespace LabManager.Database.Context
             modelBuilder.Entity<HaveTutored>().Property(x => x.Hours).HasColumnType("decimal");
 
             modelBuilder.Entity<Course>().HasKey("Code");
-            modelBuilder.Entity<Tutor>().HasKey("Ssn");
-            modelBuilder.Entity<TutoringSession>().HasKey("Code", "StartTime", "EndTime");
             modelBuilder.Entity<PlanToTutor>().HasKey("Ssn", "Code", "StartTime", "EndTime");
             modelBuilder.Entity<HaveTutored>().HasKey("Ssn", "Code", "StartTime", "EndTime");
+            modelBuilder.Entity<Tutor>().HasKey("Ssn");
+            modelBuilder.Entity<TutoringSession>().HasKey("Code", "StartTime", "EndTime");
 
-            //modelBuilder.Conventions.Add(new ForeignKeyNamingConvention());
-
-            //modelBuilder.Entity<TutoringSession>()
-            //    .HasMany(ts => ts.PlanToTutor)
-            //    .WithMany(t => t.PlanToTutor)
-            //    .Map(mc =>
-            //    {
-            //        mc.MapLeftKey("code", "startTime", "endTime");
-            //        mc.MapRightKey("ssn");
-            //        mc.ToTable("PlanToTutor");
-            //    });
         }
 
         public DbSet<Course> Course { get; set; }
 
-        public DbSet<Tutor> Tutor { get; set; }
-
-        public DbSet<TutoringSession> TutoringSession { get; set; }
-
         public DbSet<HaveTutored> HaveTutored { get; set; }
 
         public DbSet<PlanToTutor> PlanToTutor { get; set; }
+
+        public DbSet<Tutor> Tutor { get; set; }
+
+        public DbSet<TutoringSession> TutoringSession { get; set; }
     }
 }
